35c171c302c706317aa095a7e5bc965d3532e26d
weng lin <wenglin@buffalo.edu>
2019-11-22 22:27:13 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-def display_rainbow(["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"])# Write your #display_rainbow method here
+def display_rainbow(colors = ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"])# Write your #display_rainbow method here
 
 end
